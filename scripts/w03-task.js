8361205f550910eb338d9c5ba4d6f6e8602ff897
RebecaMurcia <lop17010@byui.edu>
2023-11-20 22:42:08 -0800

    w3

---
+++ scripts/w03-task.js
@@ -0,0 +1,40 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add (number1, number2) {
+    return number1 + number2 ;
+}
+let addNumbers = function (add1, add2) {
+    addNumber1 = Number(document.querySelector("#ad1").value);
+    addNumber2 = Number(document.querySelector("#ad2").value);
+
+    document.querySelector("sum").value = add(addNumber1, addNumber2);
+
+    document.querySelector("#addNumbers").addEventListener("click",addNumbers);
+}
+ 
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
