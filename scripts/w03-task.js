e0fd098d691084635256c395456434ae4773124d
RebecaMurcia <lop17010@byui.edu>
2023-11-22 15:52:35 -0800

    w3

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -66,6 +66,7 @@ let calculateTotal() {
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
+let numbersArray = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 
 /* Output Odds Only Array */
 
