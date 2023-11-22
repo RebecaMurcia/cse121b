41e4c7f311eabba995fcbbb8254e0a0a19f0ba62
RebecaMurcia <lop17010@byui.edu>
2023-11-21 20:48:37 -0800

    w3

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -2,7 +2,16 @@
 
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
+function add (number1, number2){
+return number1 + number2;
+}
+function addNumbers (addNumber1, addNumber2){
+let addNumber1 = Number(document.querySelector("#add1").value);
+let addNumber2 = Number(document.querySelector("#add2").value);
 
+document.querySelector("#sum").value = add(addNumber1, addNumber2);
+}
+document.querySelector('#adNumbers').addEventListener("click", addNumbers);
 
 
 /* Function Expression - Subtract Numbers */
