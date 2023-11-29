4956e8c4d073fd289067fcc4cd6c94a2d9d2a1ba
RebecaMurcia <lop17010@byui.edu>
2023-11-28 21:39:24 -0800

    w4

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,28 +1,80 @@
 /* LESSON 3 - Programming Tasks */
 
 /* Profile Object  */
-
-
-
+let myProfile = {
+    name: "Rebeca Murcia",
+    photo: "images/profile.jpg",
+    favoriteFoods: [
+        "avocado toast",
+        "fried eggs",
+        "arepas",
+        "enchiladas",
+        "cheesecake",
+    ],
+    hobbies: [
+        "miniature art",
+        "scarpbooking",
+        "digital illustration",
+        "mending clothes",
+    ],
+    placesLived: [],
+} ;
 
 /* Populate Profile Object with placesLive objects */
-
-
-
+myProfile.placesLived.push(
+    {
+        place: "Guatemala City, Guatemala",
+        length: "24 years",
+    },
+    {
+        place: "Los Angeles, CA",
+        length: "18 months",
+    },
+    {
+        place: "Provo,UT",
+        length: "3 years",
+    },
+    {
+        place: "Brush Prairie, WA",
+        length: "9 months"
+    },
+    {
+        place:"Portland, OR",
+        length:"1 year",
+    },
+    );
+    
 
 /* DOM Manipulation - Output */
 
 /* Name */
-
+document.querySelector("#name").textContent = myProfile.name;
 /* Photo with attributes */
-
+document.getElementById("photo").src="images/profile.jpg";
+document.getElementById("photo").alt="Rebeca Murcia";
 
 /* Favorite Foods List*/
-
+myProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement('li');
+    li.textContent = food;
+    document.querySelector("#favorite-foods").appendChild(li);
+});
 
 /* Hobbies List */
-
+myProfile.hobbies.forEach(hobbies => {
+    let li = document.createElement('li');
+    li.textContent = hobbies;
+    document.querySelector("#hobbies").appendChild(li);
+});
 
 /* Places Lived DataList */
+myProfile.placesLived.forEach(place => {
+    let dt = document.createElement ('dt');
+    dt.textContent = place.place;
+    document.querySelector("#places-lived").appendChild(dt);
 
+    let dd = document.createElement('dd');
+    dd.textContent = place.length;
+    document.querySelector('#places-lived').appendChild(dd);
+})
 
