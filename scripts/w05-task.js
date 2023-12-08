98e31dabcdd19e914cc6c481c37c7477c4b25942
RebecaMurcia <lop17010@byui.edu>
2023-12-08 00:44:46 -0800

    w5

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,7 +1,7 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-const templesElement = documet.getElementById("#temple");
+const templesElement = documet.getElementById("#temples");
 let templeList = [];
 const url = "https://byui-cse.github.io/cse121b-ww-course/resources/temples.json";
 
